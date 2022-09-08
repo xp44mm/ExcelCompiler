7b9828ce8894df9eedb60087d274e3f8fbb86f1b
xp44mm <34696643@qq.com>
2022-09-08 11:23:11 +0800

    redo generated code

--- ExcelCompiler/ExcelParsingTable.fs
+++ ExcelCompiler/ExcelParsingTable.fs
@@ -215,7 +215,7 @@ let rules:(string list*(obj list->obj))list = [
             s0 + 1
         box result
 ]
-let parser = ParserL<token>(rules,actions,closures,getTag,getLexeme)
+let parser = Parser<token>(rules,actions,closures,getTag,getLexeme)
 let parse(tokens:seq<token>) =
     tokens
     |> parser.parse
